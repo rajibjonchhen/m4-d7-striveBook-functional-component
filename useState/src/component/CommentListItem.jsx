04ed0c097a954aa9ec23a106954ab7619d4f54cc
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-11 23:46:48 +0000

    headers

--- useState/src/component/CommentListItem.jsx
+++ useState/src/component/CommentListItem.jsx
@@ -22,7 +22,7 @@ const CommentList =({comment})=>{
             try {
                 let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + id, {
                     method:"DELETE",
-                    header:{
+                    headers:{
                     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2NDE4Mjc1NzYsImV4cCI6MTY0MzAzNzE3Nn0.jwiNMWRpg2y2Ole2--KiD0VnvoMTRx8BcxRRPXSl84A"
                     }
                 })
