c18679b686b7133ab6ac070a5a1c254b6475fb38
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-11 15:10:51 +0000

    comment list also converted to function

--- useState/src/component/CommentList.jsx
+++ useState/src/component/CommentList.jsx
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 import CommentListItem from './CommentListItem'
 import {ListGroup} from 'react-bootstrap'
 
-class CommentList extends Component{
-    state={
-       comments:[],
-        isLoading:true,
-    }
-  
+const CommentList =({asin}) =>{
+   
+    const [ comments,setComments]= useState([])
+    const [ isLoading,setIsLoading]= useState(true)
 
-     componentDidUpdate = async(prevProps,prevState) => {
-         if(prevProps.asin !== this.props.asin){
+
+    useEffect(()=>{
+        fetchData()
+    },[asin])
+
+     const fetchData = async() => {
+         console.log('useEffect triggered')
              try {
-                 let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin, {
+                 let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + asin, {
                      method:"GET",   
                      headers: { 
                          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2NDE4Mjc1NzYsImV4cCI6MTY0MzAzNzE3Nn0.jwiNMWRpg2y2Ole2--KiD0VnvoMTRx8BcxRRPXSl84A"
@@ -21,24 +24,25 @@ class CommentList extends Component{
                     
                     let data = await response.json()
                     if(data){
+                        console.log(data)
                         
-                        this.setState({comments:data})  
-                        this.setState({isLoading:false})  
+                        setComments(data)  
+                        setIsLoading(false)  
                     }
                     
                 } catch (error) {
                     console.log(error)
                 }
             }
-        }
+        
 
-    render(){
+   
     return(
         <ListGroup as="ul">   
-            {this.state.comments && this.state.comments.map((comment,i) => <CommentListItem key={i} comment={comment}/>)}
+            {comments && comments.map((comment,i) => <CommentListItem key={i} comment={comment}/>)}
         </ListGroup>
     )
-    }
+    
 }
 
 export default CommentList
